e5dc581ce6bd77c46b12b840e7a2358055f12144
Fedor Ushakov <ushakovfedor@yandex-team.ru>
2019-01-26 11:43:15 +0300

    Updated clean-css up to 4.2.1

--- index.js
+++ index.js
@@ -5,11 +5,7 @@ exports.Tech = cssbase.Tech.inherit({
     minimize: function(content) {
         var CleanCSS = require('clean-css');
 
-        var CleanCSSOptions = this.opts.techOptions.cleancss || {};
-        CleanCSSOptions.rebase = typeof CleanCSSOptions.noRebase === 'undefined' ? true : false;
-        CleanCSSOptions.processImport = typeof CleanCSSOptions.processImport === 'undefined' ? false : CleanCSSOptions.processImport;
-
-        return new CleanCSS(CleanCSSOptions).minify(content).styles;
+        return new CleanCSS(this.opts.techOptions.cleancss).minify(content).styles;
     }
 
 });
